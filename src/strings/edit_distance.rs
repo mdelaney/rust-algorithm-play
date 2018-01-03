508dcee6b9f844746801aafb8fbc164d6dc1cedd
Matthew Delaney <matthew@delaney.email>
2018-01-03 12:25:11 -0800

    levenshtein distance algorithm optimized
    only keeps the last row rather than compute the full matrix

--- src/strings/edit_distance.rs
+++ src/strings/edit_distance.rs
@@ -1,17 +1,12 @@
 pub fn levenshtein_distance(a: &String, b: &String) -> usize {
     let str_a: Vec<char> = a.chars().collect();
     let str_b: Vec<char> = b.chars().collect();
-
-    let mut m: Vec<Vec<usize>> = vec![vec![0; str_a.len() + 1]; str_b.len() + 1];
-
-    for col in 0..str_a.len() + 1 {
-        m[0][col] = col;
-    }
-    for row in 0..str_b.len() + 1 {
-        m[row][0] = row;
-    }
+    let mut result: Vec<usize> = (0..str_a.len() + 1).collect();
 
     for row in 1..str_b.len()+1 {
+        let last_row = result.clone();
+        result[0] = row;
+
         for col in 1..str_a.len()+1 {
             let cost = if str_a[col-1] == str_b[row-1] {
                 0
@@ -19,18 +14,15 @@ pub fn levenshtein_distance(a: &String, b: &String) -> usize {
                 1
             };
 
-            let mut min_cost = m[row-1][col] + 1;
-            if min_cost > m[row][col-1] + 1 {
-                min_cost = m[row][col-1] + 1;
-            }
-            if min_cost > m[row-1][col-1] + cost {
-                min_cost = m[row-1][col-1] + cost;
-            }
-            m[row][col] = min_cost;
+            result[col] = *[
+                last_row[col] + 1,
+                result[col-1] + 1,
+                last_row[col-1] + cost
+            ].iter().min().unwrap();
         }
     }
 
-    m[str_b.len()][str_a.len()]
+    result[str_a.len()]
 }
 
 #[cfg(test)]
